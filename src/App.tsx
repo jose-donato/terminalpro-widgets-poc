a0221b430c6105d95a16afc919f4871a0e0a9cef
jose-donato <zmcdonato@gmail.com>
2024-06-12 15:09:26 +0100

    :(

--- src/App.tsx
+++ src/App.tsx
@@ -4,25 +4,15 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-	useEffect(() => {
-		const loadWebComponents = async () => {
-			// Fetch the list of files from a directory listing endpoint or a static manifest file
-			// For demonstration, assuming a static array of filenames
-			const components = ["simple-counter.js"]; // This array should ideally be fetched from a server or built dynamically
-			for (const component of components) {
-				const modulePath = `${import.meta.env.VITE_PUBLIC_URL}/web-components/${component}`;
-				import(modulePath)
-					.then(() => console.log(`${component} loaded successfully`))
-					.catch((err) => console.error(`Failed to load ${component}:`, err));
-			}
-		};
-		loadWebComponents();
-	}, []);
-
 	return (
-		<>
-			<simple-counter></simple-counter>
-		</>
+		<div>
+			<iframe
+				src={`${import.meta.env.VITE_PUBLIC_URL}/widgets/counter.html?instance=1`}
+			/>
+			<iframe
+				src={`${import.meta.env.VITE_PUBLIC_URL}/widgets/counter.html?instance=2`}
+			/>
+		</div>
 	);
 }
 
